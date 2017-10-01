b029fc2f59a655339be91ed6f0113e14f2cbc05b
Muhammad Fauzan Albaihaqi <mfauzan.albaihaqi@gmail.com>
2017-10-01 18:44:21 +0700

    add statement at onitemclicklistener

--- app/src/main/java/com/tif/tesyant/mov/MainActivity.java
+++ app/src/main/java/com/tif/tesyant/mov/MainActivity.java
@@ -130,7 +130,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             @Override
             public void onResponse(Call<SearchMovie> call, retrofit2.Response<SearchMovie> response) {
-
                 SearchMovie mSearch = response.body();
                 int result_size = mSearch.getResults().size();
                 final String[] list_movie_id = new String[result_size];
@@ -141,40 +140,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 CustomAdapter listAdapter = new CustomAdapter(mov, MainActivity.this, new CustomItemClickListener() {
                     @Override
                     public void onItemClick(View v, int position) {
-                        Toast.makeText(MainActivity.this, "Position : " + position, Toast.LENGTH_SHORT).show();
+                        String id = list_movie_id[position];
+                        Intent intent = new Intent(MainActivity.this, DetailListActivity.class);
+                        intent.putExtra("movieId", id);
+                        startActivity(intent);
                     }
                 });
                 LinearLayoutManager llm = new LinearLayoutManager(getApplicationContext());
                 llm.setOrientation(LinearLayoutManager.VERTICAL);
                 recyclerView.setLayoutManager(llm);
                 recyclerView.setAdapter(listAdapter);
-//                recyclerView.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
-//                    @Override
-//                    public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
-//                        return false;
-//                    }
-//
-//                    @Override
-//                    public void onTouchEvent(RecyclerView rv, MotionEvent e) {
-//
-//                    }
-//
-//                    @Override
-//                    public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
-//
-//                    }
-//                });
-//                recyclerView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//                    @Override
-//                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-//                        String id = list_movie_id[i];
-//                        Toast.makeText(MainActivity.this, "ID = " + id, Toast.LENGTH_SHORT).show();
-//                        Intent intent = new Intent(MainActivity.this, DetailListActivity.class);
-//                        intent.putExtra("movieId", id);
-//                        startActivity(intent);
-//                    }
-//                });
-//                listView.setVisibility(View.VISIBLE);
             }
 
             @Override
