300af36d647a965100f9a7a1dc65587641ddc78c
tesya <tesyant@gmail.com>
2017-10-04 12:00:59 +0700

    adding settings

--- app/src/main/java/com/tif/tesyant/mov/SettingsActivity.java
+++ app/src/main/java/com/tif/tesyant/mov/SettingsActivity.java
@@ -8,6 +8,5 @@ public class SettingsActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
     }
 }
